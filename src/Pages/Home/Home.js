878f05aff38626e6fa0f22e0e95243cf4572130f
aviafrid781 <aviafrid781@gmail/com>
2022-08-04 03:07:39 +0600

    up

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -22,179 +22,186 @@ import { TbPuzzle } from "react-icons/tb";
 import { AiOutlineTrophy } from 'react-icons/ai';
 const Home = () => {
     let iconStyles = { color: " hsla(220, 28%, 40%, 1)", fontSize: "2.0em", margin: "10px" };
+
     return (
         <div className='main-contrainer'>
+            <div>
+                <div className='parent-div'>
+
+                     <div className='container'> 
+
+                        <div className='sub-contrainer'> 
+
+                            <div className='header-fixed'>
+
+                                <div className='header-img'>
+
+                                    <img src={logo} alt="" />
+
+                                </div>
+
+                                <div className='header-text'>
+
+                                    <h1>ইশতিয়াক</h1>
+
+                                    <p>Level-Warrior</p>
+                                </div>
+
+                            </div>
 
 
-            <div className='container'>
+                            <div>
+                                < Nav.Link as={Link} to="reward">
+                                    <Button variant="hsla(223, 66%, 87%, 1)"> <AiOutlineTrophy style={{ fontSize: "1.2em", marginRight: "4px" }} />রিওয়ার্ড</Button>{' '}
+                                </Nav.Link>
+                            </div>
 
-                <div className='sub-contrainer'>
-
-                    <div className='header-fixed'>
-
-                        <div className='header-img'>
-
-                            <img src={logo} alt="" />
-
-                        </div>
-
-                        <div className='header-text'>
-
-                            <h1>ইশতিয়াক</h1>
-
-                            <p>Level-Warrior</p>
                         </div>
 
                     </div>
 
 
-                    <div>
-                        < Nav.Link as={Link} to="reward">
-                            <Button variant="hsla(223, 66%, 87%, 1)"> <AiOutlineTrophy style={{fontSize: "1.2em", marginLeft: "3px"}} />রিওয়ার্ড</Button>{' '}
-                        </Nav.Link>
+                    <div className='caontainer-point'>
+
+                        <div className='text-point'>
+                            <p>আপনার লেভেল : Warrior</p>
+                            <h1>রিওয়ার্ড পয়েন্ট ঃ ১৫০০</h1>
+                        </div>
+
+                        <div className='logo-point'>
+                            <img src={log2} alt="" />
+                        </div>
+
+                    </div>
+
+
+                    {/* slider show */}
+
+                    <div className='slider'>
+                        <Carousel fade>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={slide}
+                                    alt="First slide"
+                                />
+
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={slide}
+                                    alt="Second slide"
+                                />
+
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={slide}
+                                    alt="Third slide"
+                                />
+
+                            </Carousel.Item>
+                        </Carousel>
+                    </div>
+
+                    {/* quiz cart show */}
+
+                    <h1 className='card-header'>কুইজ ক্যাটাগরি পছন্দ করুন</h1>
+
+
+
+                    <div className='card-container'>
+
+
+                        <Row>
+                            <Col xs={4} md={4}>
+
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={medel} />
+                                    <Card.Body>
+                                        <p>খেলাধুলা</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={book} />
+                                    <Card.Body>
+                                        <p>তথ্যমূলক</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={gogle} />
+                                    <Card.Body>
+                                        <p>গুগলি</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+
+
+                            <Col xs={4} md={4}>
+
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={globe} />
+                                    <Card.Body>
+                                        <p>আন্তর্জাতিক</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={bd} />
+                                    <Card.Body>
+                                        <p>বাংলাদেশ</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <Card className='card'>
+                                    <Card.Img variant="top" src={voltage} />
+                                    <Card.Body>
+                                        <p>এন্টারটেইন</p>
+
+                                    </Card.Body>
+
+                                </Card>
+                            </Col>
+                        </Row>
+
+
+                    </div>
+
+                    {/* footer  */}
+
+                    <div className='footer-contrainer'>
+
+                        <div className='footer'>
+
+                            <AiFillHome style={iconStyles} />
+                            < BiTrophy style={iconStyles} />
+                            <TbPuzzle style={iconStyles} />
+                            < HiOutlineMenu style={iconStyles} />
+
+                        </div>
                     </div>
 
                 </div>
 
-            </div>
-
-            <div className='caontainer-point'>
-
-                <div className='text-point'>
-                    <p>আপনার লেভেল : Warrior</p>
-                    <h1>রিওয়ার্ড পয়েন্ট ঃ ১৫০০</h1>
-                </div>
-
-                <div className='logo-point'>
-                    <img src={log2} alt="" />
-                </div>
-
-            </div>
-
-
-            {/* slider show */}
-
-            <div className='slider'>
-                <Carousel fade>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={slide}
-                            alt="First slide"
-                        />
-
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={slide}
-                            alt="Second slide"
-                        />
-
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={slide}
-                            alt="Third slide"
-                        />
-
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-
-            {/* quiz cart show */}
-
-            <h1 className='card-header'>কুইজ ক্যাটাগরি পছন্দ করুন</h1>
-
-
-
-            <div className='card-container'>
-
-
-                <Row>
-                    <Col xs={4} md={4}>
-
-                        <Card className='card'>
-                            <Card.Img variant="top" src={medel} />
-                            <Card.Body>
-                                <p>খেলাধুলা</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='card'>
-                            <Card.Img variant="top" src={book} />
-                            <Card.Body>
-                                <p>তথ্যমূলক</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='card'>
-                            <Card.Img variant="top" src={gogle} />
-                            <Card.Body>
-                                <p>গুগলি</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-
-
-                    <Col xs={4} md={4}>
-
-                        <Card className='card'>
-                            <Card.Img variant="top" src={globe} />
-                            <Card.Body>
-                                <p>আন্তর্জাতিক</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='card'>
-                            <Card.Img variant="top" src={bd} />
-                            <Card.Body>
-                                <p>বাংলাদেশ</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                    <Col xs={4} md={4}>
-                        <Card className='card'>
-                            <Card.Img variant="top" src={voltage} />
-                            <Card.Body>
-                                <p>এন্টারটেইন</p>
-
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                </Row>
-
-
-            </div>
-
-            {/* footer  */}
-
-            <div className='footer-contrainer'>
-
-                <div className='footer'>
-
-                    <AiFillHome style={iconStyles} />
-                    < BiTrophy style={iconStyles} />
-                    <TbPuzzle style={iconStyles} />
-                    < HiOutlineMenu style={iconStyles} />
-
-                </div>
-            </div>
+             </div> 
 
         </div >
 
